9e3d7224f046b8940701ff7e190953959e5671a7
sawa-zen <sawadasuiren@gmail.com>
2024-07-17 00:50:36 +0900

    fixup! fixup! fixup! fixup! first

--- app/components/Megabeam/fragment.glsl
+++ app/components/Megabeam/fragment.glsl
@@ -1,18 +1,18 @@
-uniform vec2 u_resolution;
+uniform vec2 u_resolution; // window のサイズ情報
 uniform float u_time; // frame 数
 varying vec2 vUv; // fragmentShaderに渡すためのvarying変数
 
 void main() {
-  vec2 st = vUv * u_resolution;
+  vec2 st = vUv * u_resolution; // window のサイズに合わせた座標に変換
   st.x *= u_resolution.x / u_resolution.y;
 
-  float barWidth = 4.0;
-  float speed = 50.0;
-  float waveFrequency = 2.0;
-  float waveAmplitude = 20.0;
-  float wave = sin(st.x * waveFrequency + u_time * speed) * waveAmplitude;
+  float barWidth = 4.0; // バーの幅
+  float speed = 0.1; // バーのスピード
+  float waveFrequency = 1.0; // 波の周波数
+  float waveAmplitude = 10.0; // 波の振幅
+  float wave = sin(st.x * waveFrequency + u_time * speed) * waveAmplitude; // 波の計算
 
-  float color = step(5.0 + wave, mod(st.x + u_time * speed, barWidth * 2.0));
+  float color = step(5.0 + wave, mod(st.x + u_time * speed, barWidth * 2.0)); // バーの色
 
-  gl_FragColor = vec4(vec3(0.0, color, color), 1.0);
+  gl_FragColor = vec4(vec3(0.0, color, color), 1.0); // バーの色
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
           <sphereGeometry args={[100, 60, 40]} />
           <meshBasicMaterial side={2} color="skyblue" />
         </mesh>
-        <MegaBeam position={[0, 0, 0]} />
+        <MegaBeam position={[0, 0, 4]} />
       </Canvas>
     </div>
   );
